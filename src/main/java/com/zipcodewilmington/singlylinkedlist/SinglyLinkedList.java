dd9f9d25046cc822ab4f5d17842bd0ae2af85c9f
DestanyM <destanymcclellan22@gmail.com>
2023-07-28 12:24:14 -0400

    created some linked lists

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,47 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    LinkedList<String> linkedList = new LinkedList<>();
+
+    public void add(){
+        linkedList.add("John");
+        linkedList.add("Mike");
+        linkedList.add("Eric");
+        linkedList.add("Ryan");
+        linkedList.add("Josh");
+
+    }
+    public void remove(){
+        linkedList.remove(0);
+    }
+
+    public void contains(){
+        linkedList.contains("Mike");
+    }
+
+    public void find(){
+        //linkedList.find("Mike");
+    }
+
+    public void size(){
+        linkedList.size();
+    }
+
+    public void get(){
+        linkedList.get(0);
+    }
+
+    public void copy(){
+       // linkedList.copy();
+
+    }
+
+    public void sort(){
+       // linkedList.sort();
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,19 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Test;
+
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void testAdd(){
+        LinkedList<String> linkedList = new LinkedList<String>();
+
+
+   }
+
 }
